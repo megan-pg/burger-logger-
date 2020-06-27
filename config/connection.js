e5fa64a584b2303dbc8e47a618f9cb346276c088
megan-pg <megan.pardygokcu@unh.edu>
2020-06-26 21:01:01 -0400

    hooking up JAWS_DB and heroku

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,18 @@
 const mysql = require('mysql');
 
-const connection = mysql.createConnection({
-    host: 'localhost',
-    port: 8080,
-    user: 'root',
-    password: 'Happy1786!!',
-    database: 'burger_db'
-});
+const connection;
 
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: 'localhost',
+        port: 8080,
+        user: 'root',
+        password: 'Happy1786!!',
+        database: 'burger_db'
+    });
+};
 
 connection.connect(function (err) {
     if (err) {
@@ -17,4 +22,5 @@ connection.connect(function (err) {
     console.log("connected as id: " + connection.threadId);
 });
 
+connection.connect();
 module.exports = connection; 
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,1 +1,24 @@
-INSERT INTO burger(burger_name, devoured) VALUES ('cheeseburger', true); 
+INSERT INTO burger
+    (burger_name, devoured)
+VALUES
+    ('cheeseburger', false);
+INSERT INTO burger
+    (burger_name, devoured)
+VALUES
+    ('baconburger', false);
+INSERT INTO burger
+    (burger_name, devoured)
+VALUES
+    ('texmexburger', false);
+INSERT INTO burger
+    (burger_name, devoured)
+VALUES
+    ('beanburger', false);
+INSERT INTO burger
+    (burger_name, devoured)
+VALUES
+    ('mushroomburger', false);
+INSERT INTO burger
+    (burger_name, devoured)
+VALUES
+    ('chickenburger', false); 
